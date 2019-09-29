21efd69d32ab7ca7f6f65de4f8bd1efbae9937f3
SShadowRavenSS <47855765+SShadowRavenSS@users.noreply.github.com>
2019-09-29 23:27:13 +0200

    Commented GameEngine
    Commented GameEngine class and tidied up the code

--- ReeceNewman_19011948_GADE1B_POE/ReeceNewman_19011948_GADE1B_POE/GameEngine.cs
+++ ReeceNewman_19011948_GADE1B_POE/ReeceNewman_19011948_GADE1B_POE/GameEngine.cs
@@ -8,24 +8,24 @@ namespace ReeceNewman_19011948_GADE1B_POE
 {
     class GameEngine
     {
+        //Variable declarations
         private Map map;
         int counter = 0;
         public Map Map { get => map; }
 
-
+        //Constructor for gameEngine
         public GameEngine(int numberOfUnits)
         {
-
+            //Creates map
             map = new Map(numberOfUnits, 20, 20);
             map.newBattlefield();
 
         }
 
+        //Method that operates the games logic and controlls units
         public void gameLogic(Unit[] unit)
         {
-
-            bool resetCounter = false;
-
+            //Loop that runs through all the units in the unit array
             for (int i = 0; i < unit.Length; i++)
             {
                 unit[i].death(); //Check for death and set relevant variable
@@ -44,31 +44,21 @@ namespace ReeceNewman_19011948_GADE1B_POE
                     else if (unit[i].Faction != closest.Faction) //If the unit is not part of the same team
                     {
                         unit[i].combat(closest); //Do combat
-                        map.populateMap(); //Refreh map
+                        map.populateMap(); //Refresh map
                     }
 
                 }
-
-
-            }
-
-
-            if (resetCounter == true)
-            {
-                resetCounter = false;
-                counter = 0;
-            }
-            else
-            {
-                ++counter;
 
             }
 
         }
 
+        //Method that returns formatted string of all the units stats
         public string getStats(Unit[] unit)
         {
             string stats = "";
+
+            //loop to run through all the units in the passed in array
             for (int i = 0; i < unit.Length; i++)
             {
                 stats += unit[i].ToString();
