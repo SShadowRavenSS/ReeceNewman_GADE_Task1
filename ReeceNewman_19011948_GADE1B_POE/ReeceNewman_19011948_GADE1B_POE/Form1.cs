5b87952b21aac4427ced0152afe28668edefd9e0
SShadowRavenSS <47855765+SShadowRavenSS@users.noreply.github.com>
2019-09-29 22:50:41 +0200

    Commented Form1
    Added comments to form1 code

--- ReeceNewman_19011948_GADE1B_POE/ReeceNewman_19011948_GADE1B_POE/Form1.cs
+++ ReeceNewman_19011948_GADE1B_POE/ReeceNewman_19011948_GADE1B_POE/Form1.cs
@@ -13,7 +13,7 @@ namespace ReeceNewman_19011948_GADE1B_POE
     public partial class frmBattleSim : Form
     {
         
-        GameEngine gameEngine;
+        GameEngine gameEngine; //Creates space in memory for object of class GameEngine to run the game
 
         public frmBattleSim()
         {
@@ -23,35 +23,35 @@ namespace ReeceNewman_19011948_GADE1B_POE
         private void frmBattleSim_Load(object sender, EventArgs e)
         {
 
-            gameEngine = new GameEngine(5);
-            lblMap.Text = gameEngine.Map.convertMap();
+            gameEngine = new GameEngine(5); //Initializes the gameEngine with its parameters (5 units)
+            lblMap.Text = gameEngine.Map.convertMap(); //Updates map text and displays it on the form
 
-            rtxUnitInfo.Text = gameEngine.getStats(gameEngine.Map.Units);
+            rtxUnitInfo.Text = gameEngine.getStats(gameEngine.Map.Units); //Updates the units stats and displays it on the form
 
 
         }
 
         
 
-        private void btnStart_Click(object sender, EventArgs e)
+        private void btnStart_Click(object sender, EventArgs e) //Enables and starts the timer on the click of start button event
         {
-            tmrOnly.Enabled = true;
+            tmrOnly.Enabled = true; 
             tmrOnly.Start();
             
         }
 
         private void playGame(object sender, EventArgs e)
         {
-            gameEngine.gameLogic(gameEngine.Map.Units);
-            lblMap.Text = gameEngine.Map.convertMap();
-            lblTimer.Text = Convert.ToString(Convert.ToInt32(lblTimer.Text) + 1);
-            rtxUnitInfo.Text = gameEngine.getStats(gameEngine.Map.Units);
+            gameEngine.gameLogic(gameEngine.Map.Units); //Calls the gamelogic method of the gameEngine to update all the logic of the units
+            lblMap.Text = gameEngine.Map.convertMap(); //Displays the updated map on the Label in the form
+            lblTimer.Text = Convert.ToString(Convert.ToInt32(lblTimer.Text) + 1); //Updates the timer text
+            rtxUnitInfo.Text = gameEngine.getStats(gameEngine.Map.Units); //Displays the updated unit info on the Label in the form
 
         }
 
         private void btnPause_Click(object sender, EventArgs e)
         {
-            tmrOnly.Stop();
+            tmrOnly.Stop(); //Stops the timer
         }
     }
 }
