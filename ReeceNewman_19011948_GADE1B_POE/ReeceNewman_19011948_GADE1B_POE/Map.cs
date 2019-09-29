7f63db63595edb3785a92041f1c9d87f1f2dd4fc
SShadowRavenSS <47855765+SShadowRavenSS@users.noreply.github.com>
2019-09-29 23:06:32 +0200

    Commented Map class
    Commented and neatend the code for the map class

--- ReeceNewman_19011948_GADE1B_POE/ReeceNewman_19011948_GADE1B_POE/Map.cs
+++ ReeceNewman_19011948_GADE1B_POE/ReeceNewman_19011948_GADE1B_POE/Map.cs
@@ -8,42 +8,47 @@ namespace ReeceNewman_19011948_GADE1B_POE
 {
     class Map
     {
+        //Create variables
         private char[,] map;
         private int mapSizeX, mapSizeY;
         Random rng = new Random();
         private Unit[] units;
         
-
+        //Create accessors for the variables
         public char[,] MapDisplay { get => map; set => map = value; }
         public int MapSizeX { get => mapSizeX; }
         public int MapSizeY { get => mapSizeY; }
         public Random Rng { get => rng; set => rng = value; }
         public Unit[] Units { get => units; set => units = value; }
 
-        public Map(int numOfUnits, int mapSizeX, int mapsizeY)
+        //Constructor for the class
+        public Map(int numOfUnits, int mapSizeX, int mapsizeY) //takes in parameters
         {
-            this.mapSizeX = mapSizeX;
+            //initializes parameters
+            this.mapSizeX = mapSizeX; 
             this.mapSizeY = mapsizeY;
             MapDisplay = new char[MapSizeY, MapSizeX];
             Units = new Unit[numOfUnits];
             
         }
 
-        
 
-        public void newBattlefield()
+        //Generates a new Battlefield from scratch
+        public void newBattlefield() 
         {
+            //declare temp variables
             int xPos, yPos, type, faction;
             
-
+            //Loop to create the units and storer them in the array
             for (int l = 0; l < Units.Length; l++)
             {
+                //Random new values
                 xPos = Rng.Next(0, 20);
                 yPos = Rng.Next(0, 20);
                 type = Rng.Next(0, 2);
                 faction = Rng.Next(0, 2);
                 
-
+                //Create units based on the type and faction with the randomed values
                 if (type == 0 && faction == 0)
                 {
                     MeleeUnit unit = new MeleeUnit(xPos,yPos,120,1,1,'M',10,faction);
@@ -64,16 +69,20 @@ namespace ReeceNewman_19011948_GADE1B_POE
                     RangedUnit unit = new RangedUnit(xPos, yPos, 100, 1, 2, 'r', 5, faction);
                     Units[l] = unit;
                 }
+
             }
 
+            //Calss the method that populates the mapdisplay 
             populateMap();
                        
         }
 
+        //Method that formats the map and returns the formatted string
         public string convertMap()
         {
             string mapOutput = "";
 
+            //loop to add all the values of the map array to a string
             for (int i = 0; i < mapSizeY; i++)
             {
                 for (int k = 0; k < mapSizeX; k++)
@@ -86,20 +95,23 @@ namespace ReeceNewman_19011948_GADE1B_POE
             return mapOutput;
         }
 
+        //Method that refreshes the map
         public void populateMap()
         {
-            
+            //loop to fill the array with 'grass' characters
             for (int i = 0; i < MapSizeY; i++)
             { 
 
                 for (int k = 0; k < MapSizeX; k++)
                 {
+
                     MapDisplay[i, k] = '.';
                    
                 }
                 
             }
 
+            //loop that inserts the units into the array
             for (int m = 0; m < Units.Length; m++)
             {
                 
@@ -109,37 +121,41 @@ namespace ReeceNewman_19011948_GADE1B_POE
             
         }
 
+        //Method that inserts the units into their coordinates on the map
         private void insertUnits(Unit unit)
         {
+            //Type check for the passed in unit
             string unitType = unit.GetType().ToString();
             string[] unitArr = unitType.Split('.');
             unitType = unitArr[unitArr.Length - 1];
 
-
             if (unitType == "MeleeUnit")
             {
+                //temp instance of the unit for use
                 MeleeUnit u = (MeleeUnit)unit;
 
                 if(u.IsDead == false)
                 {
-                    MapDisplay[u.YPos, u.XPos] = u.Symbol;
+                    //adds the unit's symbol to the array in the correct position
+                    MapDisplay[u.YPos, u.XPos] = u.Symbol; 
                 }
                 
-
             }
             else if(unitType == "RangedUnit")
             {
+                //temp instance of the unit for use
                 RangedUnit r = (RangedUnit)unit;
 
                 if(r.IsDead == false)
                 {
+                    //adds the unit's symbol to the array in the correct position
                     MapDisplay[r.YPos, r.XPos] = r.Symbol;
                 }
                 
             }
    
         }
-
-       
+ 
     }
+
 }
